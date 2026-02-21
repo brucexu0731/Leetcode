484d3a42b3b2298919c84cc42c2fb6b2395600b2
Bruce Xu <brucexu@sas.upenn.edu>
2026-02-21 17:39:44 +0800

    Time: 0 ms (100%), Space: 19.6 MB (31.64%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -8,20 +8,33 @@ class Solution:
             if nums[m] == target:
                 return m
 
-            if target < nums[m]:
-                if nums[m] < nums[l]:
-                    r = m - 1
-                elif nums[m] >= nums[l]:
-                    if target < nums[l]:
-                        l = m + 1
-                    else:
-                        r = m - 1
-            elif target > nums[m]:
-                if nums[m] >= nums[l]:
+            # when mid point is in left sorted array (if nums[m] is equal to nums[l] it is in the left sorted) 
+            if nums[m] >= nums[l]:
+                #if target is greater than mid point, we cam only seach right
+                if target > nums[m]:
                     l = m + 1
-                elif nums[m] < nums[l]:
-                    if target < nums[l]:
+                #if target is less than mid point, it's either in the left sorted array or it wrapped around
+                else:
+                    #checking if in left sorted array
+                    if target >= nums[l]:
+                        r = m - 1
+                    #it's in right sorted array
+                    else:
                         l = m + 1
+            
+            #when mid point is in the right sorted array
+            elif nums[m] < nums[l]:
+                # if target is smaller than m we can only search left 
+                if target < nums[m]:
+                    r = m - 1
+                # if target is larget than m it's either in the right sorted array or it wrapped around
+                else:
+                    #check if in right sorted array
+                    if target <= nums[r]:
+                        l = m + 1
+                    #it's in left sorted array
                     else:
                         r = m - 1
+
+
         return -1
