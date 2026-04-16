56c724820df3ec9351f0e541036787059cf36c06
Bruce Xu <brucexu@sas.upenn.edu>
2026-04-17 00:45:07 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -13,26 +13,22 @@ class Solution:
             return None 
 
         queue1 = deque()
-        queue2 = deque()
         head = Node(node.val, [])
 
         visit = {node.val: head}
         queue1.append(node)
-        queue2.append(head)
 
         while queue1:
             for i in range(len(queue1)):
-                curr1 = queue1.popleft()
-                curr2 = queue2.popleft()
-                for n1 in curr1.neighbors:
-                    if n1.val in visit:
-                        curr2.neighbors.append(visit[n1.val])
+                curr = queue1.popleft()
+                for n in curr.neighbors:
+                    if n.val in visit:
+                        visit[curr.val].neighbors.append(visit[n.val])
                         continue 
-                    n2 = Node(n1.val, [])
-                    curr2.neighbors.append(n2)
-                    queue1.append(n1)
-                    queue2.append(n2)
-                    visit[n1.val] = n2
+                    copy = Node(n.val, [])
+                    visit[curr.val].neighbors.append(copy)
+                    queue1.append(n)
+                    visit[n.val] = copy
         
         
         return head
